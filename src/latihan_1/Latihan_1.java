151a9317fa64344b5c6a6cb7f3ada63a5180e6cc
unknown <nayang_etha_29rpl@student.smktelkom-mlg.sch.id>
2021-01-08 18:37:02 +0700

    first commit

---
+++ src/latihan_1/Latihan_1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan_1;
+
+/**
+ *
+ * @author MOKLET1
+ */
+public class Latihan_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hello");
+        System.out.println("Selamat Belajar di program Java");
+    }
+    
+}
